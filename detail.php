3f396e2414a92371ac5c000b793131714233b5b4
Aulia Fasya Nugroho <liuli.fasya@gmail.com>
2015-08-18 14:12:58 +0700

    hapus button save di rincian

--- detail.php
+++ detail.php
@@ -323,7 +323,6 @@
 															  </div>
 															  <div class="modal-footer">
 																		<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-																		<button type="button" class="btn btn-primary" data-dismiss="modal">Save changes</button>
 																</div>
 															</div>
 														  </div>
